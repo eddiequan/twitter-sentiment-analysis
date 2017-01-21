eb9e3af29cc6ab5c97d13ed2728c40961b7d517f
Quan Eddie <quaneddi@cdf.toronto.edu>
2017-01-21 16:24:02 -0500

    adds text-preprocessing to script (removes html tags and character codes)

--- twitt.py
+++ twitt.py
@@ -1,6 +1,28 @@
-import time
+import csv
+import sys
+import re
+from HTMLParser import HTMLParser
+
+def process(tweet):
+    processed_tweet = encode_ascii(tweet)
+    processed_tweet = remove_html_tags(processed_tweet)
+    processed_tweet = replace_html_character_codes(processed_tweet)
+    return processed_tweet
+    
+def encode_ascii(tweet):
+    return tweet.decode('utf-8', 'ignore').encode('ascii', 'ignore')
+
+def remove_html_tags(tweet):
+    return re.sub(r'<[^>]+>', "", tweet)
+
+def replace_html_character_codes(tweet):
+    h = HTMLParser()
+    return h.unescape(tweet)
 
 if __name__ == "__main__":
-    print "Hello world"
-
-
+    with open(sys.argv[1], 'rb') as twitter_csv:
+        reader = csv.reader(twitter_csv)
+        for row in reader:
+            print(process(row[5]))
+    print "Done!"
+    
